cbbb9811cfbde370a4c273120d2120f365fd1bf2
Lord Byron <Jbayron76@gmail.com>
2025-01-01 22:09:50 +0800

    UI

---
+++ lib/UI.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Audify extends StatefulWidget {
+  const Audify({super.key});
+
+  @override
+  State<Audify> createState() => _AudifyState();
+}
+
+class _AudifyState extends State<Audify> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
